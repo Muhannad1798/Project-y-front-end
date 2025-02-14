b6af476820491b8440e326873685a47e6e2d6453
devhassan19 <devhassan19@email.com>
2025-02-14 16:53:34 +0300

    post style with muhannad

--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -1,28 +1,19 @@
-import './style/Post.css'
-const Post = ({ Posts }) => {
-  return (
-    <div className="tweets">
-      {/*Posts.map((post) => (
-          <div key={post._id} className="tweet">
-            <div className="tweet__header">
-              <h3></h3>
-              <p>{post.userID}</p>
-            </div>
-            <p className="tweet__content">{post.post}</p>
-          </div>
-        ))*/}
-      return(
-      {Posts.map((po) => (
-        <div key={po._id} className="tweet">
-          <div className="tweet__header">
-            <p>{po.userID}</p>
-            <p className="tweet__content">{po.post}</p>
-          </div>
-        </div>
-      ))}
-      )
-    </div>
-  )
-}
+import './style/Post.css';
 
-export default Post
+const Post = ({ posts }) => {
+  return (
+    <>
+      <div className="tweets">
+        {posts.map((post) => (
+          <div key={post.userID} className="tweet">
+            <div className="tweet__header">
+              <h3>{post.post}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Post;
